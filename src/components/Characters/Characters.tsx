8742d85dd30f0bd09321b63418298d6c7a3060eb
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-03-10 16:15:31 +0100

    🚚  Move house characters

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,11 +1,38 @@
 import React, { useState } from 'react'
-import { getOthers, getStaff, getStudents, searchByName } from '../../api/apiCalls'
+import Select from 'react-select'
+import { getHouseData, getOthers, getStaff, getStudents, searchByName } from '../../api/apiCalls'
 import styles from './Characters.module.css'
 import CharacterTable from './CharacterTable'
 
 function Characters() {
   const [data, setData] = useState(null)
   const [input, setInput] = useState('')
+  const houses = [
+    { value: 'gryffindor', label: 'Gryffindor' },
+    { value: 'hufflepuff', label: 'Hufflepuff' },
+    { value: 'ravenclaw', label: 'Ravenclaw' },
+    { value: 'slytherin', label: 'Slytherin' }
+  ]
+
+  const customStyles = {
+    option: (provided: any, state: any) => ({
+      ...provided,
+      borderBottom: '1px dotted black',
+      color: state.isFocused ? 'white' : '#353746',
+      backgroundColor: state.isFocused ? '#353746' : 'white',
+      padding: 20
+    }),
+    control: (provided: any) => ({
+      ...provided,
+      width: 200,
+      fontSize: 14
+    }),
+    singleValue: (provided: any, state: any) => {
+      const opacity = state.isFocused ? 0.5 : 1
+      const transition = 'opacity 300ms'
+      return { ...provided, opacity, transition }
+    }
+  }
 
   const handleGetStudents = async () => {
     const result = await getStudents()
@@ -27,14 +54,20 @@ function Characters() {
     setData(result.data)
   }
 
+  const handleGetHouseData = async (selectedOption: any) => {
+    const result = await getHouseData(selectedOption.value)
+    setData(result.data)
+  }
+
   return (
     <>
       <h3>Character options</h3>
       <div className={styles.optionsContainer}>
-        <div>Hogwarts students</div>
-        <div>Hogwarts staff</div>
+        <div>Students</div>
+        <div>Staff</div>
         <div>Others</div>
         <div>Search by name:</div>
+        <div>Characters per house</div>
         <button type="button" className={styles.button} onClick={handleGetStudents}>
           Show
         </button>
@@ -56,6 +89,7 @@ function Characters() {
             Search
           </button>
         </div>
+        <Select options={houses} styles={customStyles} onChange={handleGetHouseData} />
       </div>
       {data && (
         <div className={styles.tableContainer}>
--- src/components/Houses/Houses.tsx
+++ src/components/Houses/Houses.tsx
@@ -1,41 +1,9 @@
 import React, { useState } from 'react'
-import Select from 'react-select'
-import { getHouseData, getHouseHeads, getTotalStudents } from '../../api/apiCalls'
+import { getHouseHeads, getTotalStudents } from '../../api/apiCalls'
 import styles from './Houses.module.css'
 
 function Houses() {
   const [data, setData] = useState(null)
-  const houses = [
-    { value: 'gryffindor', label: 'Gryffindor' },
-    { value: 'hufflepuff', label: 'Hufflepuff' },
-    { value: 'ravenclaw', label: 'Ravenclaw' },
-    { value: 'slytherin', label: 'Slytherin' }
-  ]
-
-  const customStyles = {
-    option: (provided: any, state: any) => ({
-      ...provided,
-      borderBottom: '1px dotted black',
-      color: state.isFocused ? 'white' : '#353746',
-      backgroundColor: state.isFocused ? '#353746' : 'white',
-      padding: 20
-    }),
-    control: (provided: any) => ({
-      ...provided,
-      width: 200,
-      fontSize: 14
-    }),
-    singleValue: (provided: any, state: any) => {
-      const opacity = state.isFocused ? 0.5 : 1
-      const transition = 'opacity 300ms'
-      return { ...provided, opacity, transition }
-    }
-  }
-
-  const handleGetHouseData = async (selectedOption: any) => {
-    const result = await getHouseData(selectedOption.value)
-    console.log(result.data)
-  }
 
   const handleGetHouseHeads = async () => {
     const result = await getHouseHeads()
@@ -53,14 +21,14 @@ function Houses() {
       <div className={styles.optionsContainer}>
         <div>House heads</div>
         <div>Total students per house</div>
-        <div>Students in house</div>
+        {/* <div>Students in house</div> */}
         <button type="button" className={styles.button} onClick={handleGetHouseHeads}>
           Show
         </button>
         <button type="button" className={styles.button} onClick={handleGetTotalStudents}>
           Show
         </button>
-        <Select options={houses} styles={customStyles} onChange={handleGetHouseData} />
+        {/* <Select options={houses} styles={customStyles} onChange={handleGetHouseData} /> */}
       </div>
     </>
   )
