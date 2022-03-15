fca900b6ac67081278884b88b796216fe5a872b3
Rebecca Axelsson <ra223ai@student.lnu.se>
2022-03-15 08:56:14 +0100

    ✨ Add total students table

--- src/components/Houses/Houses.tsx
+++ src/components/Houses/Houses.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import { getHouseHeads, getTotalStudents } from '../../api/apiCalls'
 import styles from './Houses.module.css'
+import TotalStudentsTable from './TotalStudentsTable'
 
 function Houses() {
   const [data, setData] = useState(null)
+  const [totalStudents, setTotalStudents] = useState(null)
+  const [houseHeads, setHouseHeads] = useState(null)
 
   const handleGetHouseHeads = async () => {
+    setTotalStudents(null)
     const result = await getHouseHeads()
     console.log(result.data)
   }
 
   const handleGetTotalStudents = async () => {
+    setHouseHeads(null)
     const result = await getTotalStudents()
+    setTotalStudents(result.data)
     console.log(result.data)
   }
 
@@ -30,6 +36,11 @@ function Houses() {
         </button>
         {/* <Select options={houses} styles={customStyles} onChange={handleGetHouseData} /> */}
       </div>
+      {totalStudents && (
+        <div className={styles.tableContainer}>
+          <TotalStudentsTable houses={totalStudents} />
+        </div>
+      )}
     </>
   )
 }
